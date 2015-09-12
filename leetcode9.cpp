ccd955c85f70194148029f2ced8cd95e08e5ec35
candyqinlibo <gao.hzy@unearby.com>
2015-09-12 18:04:34 +0800

    update

---
+++ leetcode9.cpp
@@ -0,0 +1,14 @@
+// leetcode9.cpp : 定义控制台应用程序的入口点。
+//
+//Determine whether an integer is a palindrome(回文对称).Do this without extra space.
+#include "stdafx.h"
+
+bool isPalindrome(int x) {
+	bool result = true;
+	return result;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
