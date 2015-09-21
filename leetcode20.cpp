3ff41db3f045d32e907cd740c42ef42b51c12ef9
candyqinlibo <gao.hzy@unearby.com>
2015-09-21 18:05:16 +0800

    update

--- leetcode20.cpp
+++ leetcode20.cpp
@@ -6,32 +6,61 @@
 //"()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 #include "stdafx.h"
 #include "string.h"
+#include "malloc.h"
 bool isValid(char* s) {
 	int length = strlen(s);
+	char* temp = (char*)malloc((length+1)*sizeof(char));
+	int count = -1;
 	for (int i = 0; i < length; i++)
 	{
-		switch (s[i])
+		if (s[i] == '(' || s[i] == '[' || s[i] == '{')
 		{
-		case '[':
-			if (s[++i] == ']')
-				i++;
-			else return false;
-			break;
-		case '(':
-			if (s[++i] == ')')
-				i++;
-			else return false;
-			break;
-		case '{':
-			if (s[++i] == '}')
-				i++;
-			else return false;
-			break;
-		default:
-			return false;
+			temp[++count] = s[i];			
+		}
+		else
+		{
+			switch (temp[count])
+			{
+			case '(':
+				if (s[i] == ')')
+				{
+					temp[count] = 0;
+					count--;
+				}
+				else
+				{
+					return false;
+				}
+				break;
+			case '{':
+				if (s[i] == '}')
+				{
+					temp[count] = 0;
+					count--;
+				}
+				else
+				{
+					return false;
+				}
+				break;
+			case '[':
+				if (s[i] == ']')
+				{
+					temp[count] = 0;
+					count--;
+				}
+				else
+				{
+					return false;
+				}
+				break;
+			default:
+				return false;
+			}
 		}
 	}
-	return true;
+	if (count == -1)return true;
+	else return false;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
